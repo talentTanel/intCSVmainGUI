492a168c901c60ceabf50fe5d39c680d0639da03
Tanel Lentso <talent@ttu.ee>
2023-02-09 21:35:45 +0200

    Can get specific region in graph by time(s)

--- GUI.py
+++ GUI.py
@@ -9,14 +9,18 @@ def main():
 # User Interface code
 def GUI(data):
     gui = tk.Tk()
-    startTime = tk.Text()
-    start = tk.Button(text="Show Graph", command=lambda: plot(data, startTime))
-    startTime.pack()
+    gui.geometry("500x500")
+    #startTime = tk.Text()
+    startTime = tk.Entry()
+    stopTime = tk.Entry()
+    startTime.place(relx= .1, rely= 0)
+    stopTime.place(relx= .1, rely= .1)
+    start = tk.Button(text="Show Graph", command=lambda: plot(data, startTime.get(), stopTime.get()))
     start.pack()
     gui.mainloop()
 
 # Graphs a .CSV file
-def plot(data, time):
+def plot(data, startTime, stopTime):
     dataLength = len(data)
     ts = []
     pl = []
@@ -30,9 +34,32 @@ def plot(data, time):
             pr.append(float(data[i][5]))
         except:
             pass #data[0] is .CSV headers
-    plt.plot(ts, pl, "-r", label="Left", rasterized=True) 
-    plt.plot(ts, pc, "-b", label="Center", rasterized=True)
-    plt.plot(ts, pr, "-k", label="Right", rasterized=True)
+    old = len(ts)
+    dif = 0
+    try:
+        ts = [x for x in ts if x >= float(startTime)] # Removing all values from timestamp before startTime
+        dif = old - len(ts)
+    except:
+        pass
+    for i in range (dif):
+        pl.pop(0) # Removing all values from pressure before startTime
+        pc.pop(0)
+        pr.pop(0)
+    old = len(ts)
+    dif = 0
+    try:
+        ts = [x for x in ts if x <= float(stopTime)] # Removing all values from timestamp after stopTime
+        dif = old - len(ts)
+    except:
+        pass
+    for i in range (dif):
+        pl.pop(len(pl)-1) # Removing all values from pressure after stopTime
+        pc.pop(len(pc)-1)
+        pr.pop(len(pr)-1)
+    
+    plt.plot(ts, pl, "-r", label="Left") 
+    plt.plot(ts, pc, "-b", label="Center")
+    plt.plot(ts, pr, "-k", label="Right")
     plt.show()
         
 # Opens a .CSV file for further processing
