83e789a19956648ba80501949747356ad617a7db
Tanel Lentso <talent@ttu.ee>
2023-02-09 15:51:51 +0200

    can plot the graph now

--- GUI.py
+++ GUI.py
@@ -1,5 +1,38 @@
+import matplotlib.pyplot as plt
+import csv
+import random
+
 def main():
     print("Hello")
+    data = readCSV()
+    plot(data)
+
+# Graphs a .CSV file
+def plot(data):
+    dataLength = len(data)
+    ts = []
+    pl = []
+    pc = []
+    pr = []
+    for i in range(dataLength):
+        try:
+            ts.append(float(data[i][0]))
+            pl.append(float(data[i][1]))
+            pc.append(float(data[i][3]))
+            pr.append(float(data[i][5]))
+        except:
+            pass #data[0] is headers
+    plt.plot(ts, pl, "-r", label="Left", rasterized=True) 
+    plt.plot(ts, pc, "-b", label="Center", rasterized=True)
+    plt.plot(ts, pr, "-k", label="Right", rasterized=True)
+    plt.show()
+        
+# Opens a .CSV file for further processing
+def readCSV():
+    file = open("C660913142637.csv","r")
+    data = list(csv.reader(file, delimiter=","))
+    file.close()
+    return data
 
 if __name__ == "__main__":
     main()
