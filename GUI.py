3a8e7db0be354a00d1bec3114559f26b790b7f05
Tanel Lentso <talent@ttu.ee>
2023-02-10 21:51:25 +0200

    cleaned up code

--- GUI.py
+++ GUI.py
@@ -2,61 +2,47 @@ import matplotlib.pyplot as plt
 import csv
 import tkinter as tk
 
+gui = tk.Tk()
+
 def main():
     data = readCSV()
+    data.pop(0)
     GUI(data)
 
 # User Interface code
 def GUI(data):
-    gui = tk.Tk()
     gui.geometry("500x500")
     #startTime = tk.Text()
     startTime = tk.Entry()
     stopTime = tk.Entry()
-    startTime.place(relx= .1, rely= 0)
-    stopTime.place(relx= .1, rely= .1)
-    start = tk.Button(text="Show Graph", command=lambda: plot(data, startTime.get(), stopTime.get()))
-    start.pack()
+    startTime.place(relx= .1, rely= .1)
+    stopTime.place(relx= .1, rely= .2)
+    start = tk.Button(text="Show Graph", command=lambda: [plt.close(), plot(data, startTime.get(), stopTime.get())])
+    start.place(relx= .1, rely= 0)
     gui.mainloop()
 
 # Graphs a .CSV file
 def plot(data, startTime, stopTime):
-    dataLength = len(data)
     ts = []
     pl = []
     pc = []
     pr = []
+    dataLength = len(data)
     for i in range(dataLength):
-        try:
-            ts.append(float(data[i][0]))
-            pl.append(float(data[i][1]))
-            pc.append(float(data[i][3]))
-            pr.append(float(data[i][5]))
-        except:
-            pass #data[0] is .CSV headers
-    old = len(ts)
-    dif = 0
-    try:
+        ts.append(float(data[i][0]))
+        pl.append(float(data[i][1]))
+        pc.append(float(data[i][3]))
+        pr.append(float(data[i][5]))
+    if(startTime.isnumeric()): 
         ts = [x for x in ts if x >= float(startTime)] # Removing all values from timestamp before startTime
-        dif = old - len(ts)
-    except:
-        pass
-    for i in range (dif):
-        pl.pop(0) # Removing all values from pressure before startTime
-        pc.pop(0)
-        pr.pop(0)
-    old = len(ts)
-    dif = 0
-    try:
+        pl = sameLength(ts,pl,"0") # Removing all values from pressure before startTime
+        pc = sameLength(ts,pc,"0")
+        pr = sameLength(ts,pr,"0")
+    if(stopTime.isnumeric()): 
         ts = [x for x in ts if x <= float(stopTime)] # Removing all values from timestamp after stopTime
-        dif = old - len(ts)
-    except:
-        pass
-    for i in range (dif):
-        pl.pop(len(pl)-1) # Removing all values from pressure after stopTime
-        pc.pop(len(pc)-1)
-        pr.pop(len(pr)-1)
-    
+        pl = sameLength(ts,pl,"end") # Removing all values from pressure after stopTime
+        pc = sameLength(ts,pc,"end")
+        pr = sameLength(ts,pr,"end")
     plt.plot(ts, pl, "-r", label="Left") 
     plt.plot(ts, pc, "-b", label="Center")
     plt.plot(ts, pr, "-k", label="Right")
@@ -68,6 +54,17 @@ def readCSV():
     data = list(csv.reader(file, delimiter=","))
     file.close()
     return data
+
+# Makes two lists the same length for graphing
+def sameLength(X, Y, type):
+    if(type == "end"):
+        while len(X) != len(Y):
+            Y.pop()
+        return Y
+    else:
+        while len(X) != len(Y):
+            Y.pop(0)
+        return Y
 
 if __name__ == "__main__":
     main()
