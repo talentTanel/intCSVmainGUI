bc157980e23d9861e342ac4376f18ed28d23c00a
Tanel Lentso <talent@ttu.ee>
2023-02-27 16:25:56 +0200

    Can now get maximum point

--- GUI.py
+++ GUI.py
@@ -7,8 +7,8 @@ import os
 import db, savedGraphs
 
 fileName=""
-insertionPointXY = []
-ann, ip = None, None
+insertionPointXY, maximumPointXY = [], []
+annIp, ip, annMax, maximum = None, None, None, None
 # User Interface
 def GUI():
     gui.geometry("1280x720")
@@ -25,6 +25,7 @@ def plot(graphData, startTime, stopTime):
     graph.plot(ts, pc, "-b", label="Center")
     graph.plot(ts, pr, "-k", label="Right")
     insertionPointDef(pl, ts)
+    maximumPoint(pl, ts)
     graph.legend()
     canvas.draw()
     toolbar.update()
@@ -104,36 +105,65 @@ def sameLength(X, pl, pc, pr, type):
             pr.pop(0)
     return pl, pc, pr
     
+def maximumPoint(pl, ts):
+    maximumPointBtn = tk.Button(
+        gui, 
+        text="Suggest maximum point",
+        command=lambda: getMaximumPoint(pl, ts)
+        )
+    maximumPointBtn.place(relx=.1, rely=.42)
+    if maximumPointXY:
+        global annMax, maximum
+        maximumt = graph.plot(maximumPointXY[0], maximumPointXY[1], "or", label="Maximum point")
+        maximum = maximumt.pop(0)
+        tsPlace = ts[len(ts)-1] / 8
+        annMax = graph.annotate("Maximum Point", xy=(maximumPointXY[0], maximumPointXY[1]), xytext=(maximumPointXY[0]-tsPlace, maximumPointXY[1]+200), color="green", arrowprops= dict(facecolor="green", headwidth=8))
+        canvas.draw()
+
+def getMaximumPoint(pl, ts):
+    maxPl = max(pl)
+    maxTs = ts[pl.index(maxPl)]
+    
+    global annMax, maximum, maximumPointXY
+    maximumt = graph.plot(maxTs, maxPl, "or", label="Maximum point")
+    maximum = maximumt.pop(0)
+    tsPlace = ts[len(ts)-1] / 8
+    annMax = graph.annotate("Maximum Point", xy=(maxTs, maxPl), xytext=(maxTs-tsPlace, maxPl+200), color="green", arrowprops= dict(facecolor="green", headwidth=8))
+    canvas.draw()
+    maximumPointXY = [maxTs, maxPl]
+
+    
+
 # Checks if an insertion point is already available, if there is - displays it
 def insertionPointDef(pl, ts):
     insertionPointBtn(pl, ts)
     if insertionPointXY:
-        global ann, ip
+        global annIp, ip
         ipt = graph.plot(insertionPointXY[0],insertionPointXY[1], "or", label="Insertion Point")
         ip = ipt.pop(0)
-        ann = graph.annotate("Insertion Point", xy=(insertionPointXY[0], insertionPointXY[1]), xytext=((ts[0]+insertionPointXY[0])/2.5,insertionPointXY[1]-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
+        annIp = graph.annotate("Insertion Point", xy=(insertionPointXY[0], insertionPointXY[1]), xytext=((ts[0]+insertionPointXY[0])/2.5,insertionPointXY[1]-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
         canvas.draw()
 
 # Gets insertion point from database if it exists
 def insertionPointFromDB(tsD, ipX, ipY):
     if ipX:
-        global ann, ip
+        global annIp, ip
         ipt = graph.plot(ipX, ipY, "or", label="Insertion Point")
         ip = ipt.pop(0)
-        ann = graph.annotate("Insertion Point", xy=(ipX, ipY), xytext=((tsD[0]+ipX)/2.5,ipY-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
+        annIp = graph.annotate("Insertion Point", xy=(ipX, ipY), xytext=((tsD[0]+ipX)/2.5,ipY-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
         canvas.draw()
 
 # Suggests an insertion point automatically by comparing a pressure to it's following pressure
 def getInsertionPointAuto(pl, ts):
     for i in range(len(pl)-1):
         if abs(pl[i] - pl[i+1]) > int(insertSlider.get()):
-                global insertionPointXY, ann, ip
-                if ann: ann.remove(), ip.remove()
+                global insertionPointXY, annIp, ip
+                if annIp: annIp.remove(), ip.remove()
                 tsIn = ts[i]
                 plIn = pl[i]
                 ipt = graph.plot(tsIn,plIn, "or", label="Insertion Point")
                 ip = ipt.pop(0)
-                ann = graph.annotate("Insertion Point", xy=(tsIn, plIn), xytext=((ts[0]+tsIn)/2.5,plIn-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
+                annIp = graph.annotate("Insertion Point", xy=(tsIn, plIn), xytext=((ts[0]+tsIn)/2.5,plIn-250), color="green", arrowprops= dict(facecolor="green", headwidth=8))
                 canvas.draw()
                 insertionPointXY = [tsIn, plIn]
                 break
